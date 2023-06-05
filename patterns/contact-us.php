fcb6173cf6b5912e224008d435f987194d7d668a
Adel Tahri <adel4tahri@gmail.com>
2023-06-05 05:54:28 +0100

    NEW: contact us pattren

---
+++ patterns/contact-us.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Title: Contact Us
+ * Slug: noon/contact-us
+ * Description: 
+ * Categories: noon, page, layout
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:cover {"overlayColor":"base-1","minHeight":20,"minHeightUnit":"rem","contentPosition":"center center","tagName":"header","align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|large","bottom":"0","right":"0","left":"0"}}}} -->
+<header class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--large);padding-right:0;padding-bottom:0;padding-left:0;min-height:20rem"><span aria-hidden="true" class="wp-block-cover__background has-base-1-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"textColor":"contrast-3","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-contrast-3-color has-text-color" style="padding-top:0;padding-right:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:heading {"level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide">Share Your Feedback<br>We Love Hearing from You!</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div></header>
+<!-- /wp:cover -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"blockGap":"var:preset|spacing|large"}},"className":"feature-boxes","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull feature-boxes" style="margin-top:0px;padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|large","padding":{"right":"0","left":"0"}}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap"}} -->
+<div class="wp-block-group alignwide" style="padding-right:0;padding-left:0"><!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:0px;margin-bottom:0px"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"width":"1px"}},"borderColor":"base-2","backgroundColor":"tertiary","className":"is-style-noon-shadow","layout":{"type":"default"}} -->
+<div class="wp-block-column is-style-noon-shadow has-border-color has-base-2-border-color has-tertiary-background-color has-background" style="border-width:1px;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"fontSize":"base"} -->
+<h3 class="wp-block-heading has-base-font-size">Address</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"contrast-2","fontSize":"text-base"} -->
+<p class="has-contrast-2-color has-text-color has-text-base-font-size">Craving a visit? Discover our handy-dandy location and drop by for a delightful experience.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"backgroundColor":"base-2","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-base-2-color has-alpha-channel-opacity has-base-2-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"style":{"typography":{"textDecoration":"underline"}},"fontSize":"text-xl"} -->
+<p class="has-text-xl-font-size" style="text-decoration:underline"><a href="#">199 North Baronial St.Dorchester Center</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"width":"1px"}},"borderColor":"base-2","backgroundColor":"tertiary","className":"is-style-noon-shadow"} -->
+<div class="wp-block-column is-style-noon-shadow has-border-color has-base-2-border-color has-tertiary-background-color has-background" style="border-width:1px;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"fontSize":"base"} -->
+<h3 class="wp-block-heading has-base-font-size">Email Address</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"contrast-2"} -->
+<p class="has-contrast-2-color has-text-color">Got a question? We're here to help! Send us a message and we'll get back to you soon.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"backgroundColor":"base-2","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-base-2-color has-alpha-channel-opacity has-base-2-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"style":{"typography":{"textDecoration":"underline"}},"fontSize":"text-xl"} -->
+<p class="has-text-xl-font-size" style="text-decoration:underline"><a href="mailto:info@company.com">info@company.com</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"width":"1px"}},"borderColor":"base-2","backgroundColor":"tertiary","className":"is-style-noon-shadow"} -->
+<div class="wp-block-column is-style-noon-shadow has-border-color has-base-2-border-color has-tertiary-background-color has-background" style="border-width:1px;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"fontSize":"base"} -->
+<h3 class="wp-block-heading has-base-font-size">Phone Numbers</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"contrast-2"} -->
+<p class="has-contrast-2-color has-text-color">Looking for someone to talk to? Reach out to us at our dedicated helpline and we'll lend an ear.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"backgroundColor":"base-2","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-base-2-color has-alpha-channel-opacity has-base-2-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"fontSize":"text-xl"} -->
+<p class="has-text-xl-font-size"><a href="Tel: 123-456-7890">+1 555 666 7773</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"blockGap":"var:preset|spacing|large"}},"backgroundColor":"base-1","className":"feature-boxes","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull feature-boxes has-base-1-background-color has-background" style="margin-top:0px;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained","contentSize":"500px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Visit us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Reach Out and Connect with Us for Any Questions, Support, or Feedback – We're Here to Provide Assistance and Ensure a Positive Experience</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:html -->
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13098.229192521807!2d5.7474287!3d34.8422217!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12f5a78446317587%3A0x113883a34ff0ee8f!2sDimaLab%2C%20SARL!5e0!3m2!1sen!2sdz!4v1685938690346!5m2!1sen!2sdz" width="100%" height="70vh" style="border:0;width:100%;height:70vh;filter:grayscale(100%);" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<!-- /wp:html --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
