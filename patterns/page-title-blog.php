291cc4aacc7f3913480230bafb72ac4e2635ad19
Adel Tahri <adel4tahri@gmail.com>
2023-06-07 01:50:36 +0100

    add another FAQ

---
+++ patterns/page-title-blog.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Title: page title blog
+ * Slug: noon/page-title-blog
+ * Description: 
+ * Categories: noon, header
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:cover {"overlayColor":"fixed-dark","minHeight":80,"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:80px"><span aria-hidden="true" class="wp-block-cover__background has-fixed-dark-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:cover {"dimRatio":50,"overlayColor":"base-2","minHeight":12,"minHeightUnit":"rem","contentPosition":"center center","isDark":false,"tagName":"header","align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","right":"0","left":"0"}}}} -->
+<header class="wp-block-cover alignfull is-light" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:12rem"><span aria-hidden="true" class="wp-block-cover__background has-base-2-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"textColor":"contrast-3","layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-contrast-3-color has-text-color" style="padding-top:0;padding-bottom:0"><!-- wp:group {"align":"wide","textColor":"contrast-3","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+<div class="wp-block-group alignwide has-contrast-3-color has-text-color"><!-- wp:heading {"level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide">Blog</h1>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Home</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>/</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Blog</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></header>
+<!-- /wp:cover -->
