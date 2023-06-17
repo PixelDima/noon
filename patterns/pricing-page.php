35df023158cacc7a3af16a8d6d1d6d72168dfc33
Adel Tahri <adel4tahri@gmail.com>
2023-06-17 21:48:25 +0100

    clean code

--- patterns/pricing-page.php
+++ patterns/pricing-page.php
@@ -6,8 +6,8 @@
  * Categories: pages, noon
  * Keywords: page, layout, design, template
  * Viewport Width: 1280
- * Block Types:
- * Post Types:
+ * Block Types: 
+ * Post Types: 
  * Inserter: true
  */
 
