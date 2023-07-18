c261421ea0a0cb202611cf4afe15854e178cbe8b
Adel Tahri <adel4tahri@gmail.com>
2023-07-18 03:43:22 +0100

    improve hero with video and fix archive template

--- patterns/hero-video.php
+++ patterns/hero-video.php
@@ -4,7 +4,7 @@
  * Slug: noon/hero-video
  * Description:
  * Categories: call-to-action, hero, featured, noon
- * Keywords: cta, header, buttons, heading, hero, feature, homepage
+ * Keywords: cta, header, buttons, heading, hero, feature, homepage, video, noon
  * Viewport Width: 1280
  * Block Types:
  * Post Types:
@@ -12,9 +12,9 @@
  */
 
 ?>
-<!-- wp:cover {"url":"https://thatbook.com/wp-content/themes/book/video-bg.mp4","id":768,"backgroundType":"video","focalPoint":{"x":0.52,"y":0.49},"minHeight":70,"minHeightUnit":"vh","gradient":"noon-gradient-dark","contentPosition":"center center","align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"bottom":"0","top":"var:preset|spacing|large","right":"0","left":"0"}}}} -->
-<div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--large);padding-right:0;padding-bottom:0;padding-left:0;min-height:70vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim wp-block-cover__gradient-background has-background-gradient has-noon-gradient-dark-gradient-background"></span><video class="wp-block-cover__video-background intrinsic-ignore" autoplay muted loop playsinline src="https://thatbook.com/wp-content/themes/book/video-bg.mp4" style="object-position:52% 49%" data-object-fit="cover" data-object-position="52% 49%"></video><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide" style="padding-right:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:cover {"url":"https://thatbook.com/wp-content/themes/book/video-bg.mp4","id":768,"backgroundType":"video","focalPoint":{"x":0.52,"y":0.49},"minHeight":70,"minHeightUnit":"vh","gradient":"noon-gradient-dark","contentPosition":"center center","align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"0","left":"0"}}}} -->
+<div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-right:0;padding-left:0;min-height:70vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim wp-block-cover__gradient-background has-background-gradient has-noon-gradient-dark-gradient-background"></span><video class="wp-block-cover__video-background intrinsic-ignore" autoplay muted loop playsinline src="https://thatbook.com/wp-content/themes/book/video-bg.mp4" style="object-position:52% 49%" data-object-fit="cover" data-object-position="52% 49%"></video><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"right":"clamp(1.5rem, 4vw + .5rem, 2rem)","left":"clamp(1.5rem, 4vw + .5rem, 2rem)","top":"var:preset|spacing|xx-large","bottom":"var:preset|spacing|xx-large"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--xx-large);padding-right:clamp(1.5rem, 4vw + .5rem, 2rem);padding-bottom:var(--wp--preset--spacing--xx-large);padding-left:clamp(1.5rem, 4vw + .5rem, 2rem)"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group alignwide"><!-- wp:heading {"level":1,"align":"wide","style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"600","textTransform":"none"}},"className":"is-style-max-w-3xl","fontSize":"text-6xl"} -->
 <h1 class="wp-block-heading alignwide is-style-max-w-3xl has-text-6-xl-font-size" style="font-style:normal;font-weight:600;line-height:1.2;text-transform:none">Transform Your Website With Noon</h1>
 <!-- /wp:heading -->
